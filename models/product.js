fff8559413f62cba56404b4173b38e0144ef7ae3
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2023-08-14 14:41:38 +0530

    de

--- models/product.js
+++ models/product.js
@@ -1,1 +1,16 @@
-module
+const products = [];
+
+module.exports = class Product{
+    constructor(t){
+        this.title = t;
+    }
+    save(){
+        products.push(this);
+    }
+
+    static fetchAll(){
+        return products;
+    }
+
+
+}
