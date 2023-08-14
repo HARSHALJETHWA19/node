d22af5c3df17d3cc804f4ac8a64e2bce35d279ff
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2023-08-14 14:33:17 +0530

    new

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,9 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
 const productsController = require('../controllers/products');
 
 const router = express.Router();
-
 
 // /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
@@ -14,4 +12,4 @@ router.get('/add-product', productsController.getAddProduct);
 // /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
 
-module.exports = router; 
+module.exports = router;
