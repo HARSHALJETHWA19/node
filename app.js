cc7b5951e0f468bd32a139d18b69c47760f46243
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2023-09-14 14:02:55 +0530

    update

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorCOntroller = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorCOntroller.get404);
+app.use(errorController.get404);
 
 app.listen(3000);
