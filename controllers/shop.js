b130642cd5a44da96d9845ccc3133dbfeff59027
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2023-09-14 14:23:52 +0530

    dde

--- controllers/shop.js
+++ controllers/shop.js
@@ -30,6 +30,12 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
+exports.postCart = (req,res,next) => {
+  const prodId = req.body.productid;
+  console.log(prodId);
+  res.redirect('/cart');
+};
+
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
